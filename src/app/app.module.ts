6786a673b26d916a3b171a0597603dc27afa0473
Name <comteprovisoire@gmail.com>
2022-02-18 10:31:21 +0100

    Card - création du composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { CocktailCardComponent } from './cocktail-card/cocktail-card.component';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    // CocktailCardComponent,
+    AppComponent,
+    HeaderComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
