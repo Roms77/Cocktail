0aeb83b47e67de6bf77ddc6a568e109338699f0f
kiman <annabelle.bosse>
2022-02-18 11:00:34 +0100

    sauvegarde

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent
-  ],
+ 
   imports: [
     BrowserModule,
     AppRoutingModule
